f933d2a1b4282c3a3eb1b1f85ac90dfa4629ee44
Ian Owuor <owuorian95@gmail.com>
2024-03-02 11:16:43 +0300

    Using Recursion

--- fibonacci.js
+++ fibonacci.js
@@ -1,9 +1,10 @@
-// Implimentation Using a For Loop
+// Using a For Loop
 
 let prev2 = 0;
 let prev1 = 1;
 let newFibo;
 
+console.log("Fibonacci using For Loop");
 console.log(prev2);
 console.log(prev1);
 
@@ -13,3 +14,23 @@ for (let i = 0; i <= 17; i++) {
     prev2 = prev1;
     prev1 = newFibo;
 }
+
+// Using Recursion
+
+function printNumbers (number) {
+    console.log(number);
+}
+
+function fibonacci (prev1, prev2, count = 0) {
+    if (count <= 19) {
+        const newfibo = prev1 + prev2;
+        printNumbers(newfibo);
+
+        fibonacci (newfibo, prev1, count + 1);
+    }
+}
+
+console.log("Fibonacci using Recursion")
+printNumbers(0);
+printNumbers(1);
+fibonacci(1, 0);
