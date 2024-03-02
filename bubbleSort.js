da0a9dd2d3194b8691cba3063c58a70cb0fb23a0
Ian Owuor <owuorian95@gmail.com>
2024-03-02 16:23:23 +0300

    Bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,12 @@
+let numbers = [7, 4, 9, 2, 5, 4, 2, 7, 8, 11];
+let n = numbers.length;
+
+for (let i = 0; i < n -1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+        if (numbers[j] > numbers[j + 1]) {
+            [numbers[j], numbers[j + 1]] = [numbers[j + 1], numbers[j]];
+        }
+    }
+}
+
+console.log(`Sorted array: ${numbers}`);
