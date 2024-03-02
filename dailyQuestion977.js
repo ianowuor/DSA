0dc6817e3cd03e3ca5bdb491a34fd44352d9fdd2
Ian Owuor <owuorian95@gmail.com>
2024-03-02 23:35:48 +0300

    Leetcode 229. Majority Element II

--- dailyQuestion977.js
+++ dailyQuestion977.js
@@ -1,30 +1,3 @@
-// function sortedSquares(nums) {
-//     let left = 0;
-//     let right = nums.length - 1;
-//     let result = [];
-
-//     while (left < right) {
-//         const leftSquared = nums[left] * nums[left];
-//         const rightSquared = nums[right] * nums[right];
-
-//         if (leftSquared >= rightSquared) {
-//             result.push(leftSquared);
-//             left++;
-//         } else {
-//             result.push(rightSquared);
-//             right--;
-//         }
-//     }
-
-//     // result.reverse();
-//     return result;
-// }
-
-// console.log(sortedSquares([-4,-1,0,3,10]));
-
-// The output of the code above when tested with the input nums = [-4,-1,0,3,10] is [ 1, 9, 16, 100 ]
-// Change the code to give the output [0,1,9,16,100] when tested with the input nums = [-4,-1,0,3,10]
-
 let sortedSquares = function (nums) {
     const n = nums.length;
     let result = new Array(n);
@@ -51,3 +24,43 @@ let sortedSquares = function (nums) {
 }
 
 console.log(sortedSquares([-4,-1,0,3,10]));
+
+// 229. Majority Element II
+
+let majorityElement = function (nums) {
+    const n = nums.length;
+    const threshHold = Math.floor(n / 3);
+    const result = [];
+
+    let candidate1 = null;
+    let candidate2 = null;
+    let count1 = 0;
+    let count2 = 0;
+
+    for (const num of nums) {
+        if (num === candidate1) {
+            count1++;
+        } else if (num === candidate2) {
+            count2++;
+        } else if (count1 === 0) {
+            candidate1 = num;
+            count1 = 1;
+        } else if (count2 === 0) {
+            candidate2 = num;
+            count2 = 1;
+        } else {
+            count1--;
+            count2--;
+        }
+    }
+
+    // Verify counts and handle duplicates
+
+    for (const num of [candidate1, candidate2]) {
+        if (num !== null && nums.filter(x => x === num).length > threshHold) {
+            result.push(num);
+        }
+    }
+
+    return result;
+}
