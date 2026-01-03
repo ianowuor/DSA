1f518e9d27ccd9fcaccef8e779700406ab98b69d
Ian Owuor <owuorian95@gmail.com>
2026-01-03 20:15:20 +0300

    Better solution to encode and decode strings

--- encode_and_decode_strings.py
+++ encode_and_decode_strings.py
@@ -1,31 +1,22 @@
 class Solution:
+
     def encode(self, strs: List[str]) -> str:
-        if not strs:
-            return ""
-        sizes, res = [], ""
+        res = " ".join
+
         for s in strs:
-            sizes.append(len(s))
-        for sz in sizes:
-            res += str(sz)
-            res += ','
-        res += '#'
-        for s in strs:
-            res += s
+            res += str(len(s)) + "#" + s
+
         return res
 
     def decode(self, s: str) -> List[str]:
-        if not s:
-            return []
-        sizes, res, i = [], [], 0
-        while s[i] != '#':
-            cur = ""
-            while s[i] != ',':
-                cur += s[i]
-                i += 1
-            sizes.append(int(cur))
-            i += 1
-        i += 1
-        for sz in sizes:
-            res.append(s[i:i + sz])
-            i += sz
+        res, i = [], 0
+
+        while i < len(str):
+            j = i 
+            while str[j] != "#":
+                j += 1
+            length = int(str[i:j])
+            res.append(str[j + 1: j + 1 + length])
+            i = j + 1 + length
+
         return res
